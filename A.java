705ea2c4137f2a9c5b1b5de7134cecd815171485
sujeet-web <sujeetsharma675@gmail.com>
2020-10-16 22:38:10 +0530

    Updated file

--- A.java
+++ A.java
@@ -1,12 +1,9 @@
 public class A {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hi");
-		System.out.println("Hi");
-		System.out.println("Hi");
+        
+		System.out.println("Hi how are you");
+		System.out.println("Yes am fine thanks");
 		
     }
 }
